3eb7be9ec4acc61a5aceeefc39c16483503cec72
kasperharal <kasperschejbeljensen@icloud.com>
2025-09-02 17:39:12 +0000

    added file selector and some ui

--- chess/core/src/main/java/com/chess/Main.java
+++ chess/core/src/main/java/com/chess/Main.java
@@ -1,13 +1,61 @@
 package com.chess;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
 
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+    static public SpriteBatch spriteBatch;
+    static public FitViewport viewport;
+    static public Rectangle mouseColider = new Rectangle(0, 0, 1, 1);
+    static public boolean mousedown = false;
+    static FreeTypeFontGenerator generator;
+    static FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+    static public BitmapFont font;
 
     @Override
     public void create() {
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(1024, 1024);
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("Helvetica.ttf"));
+        parameter.size = 22;
+        font = generator.generateFont(parameter);
         setScreen(new MainScreen());
+    }
+
+    public static void baseInputLogic() {
+        Vector3 screenCoords = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        viewport.unproject(screenCoords);
+        mouseColider.x = screenCoords.x;
+        mouseColider.y = screenCoords.y;
+        mousedown = Gdx.input.isButtonPressed(0);
+    }
+
+    public static float getcord(float cord) {
+        return cord*16;
+    }
+
+    public static Rectangle getcords(float x, float y, float w, float h) {
+        return new Rectangle(getcord(x),getcord(y),getcord(w),getcord(h));
+    }
+
+    public static Rectangle getcords(float x, float y) {
+        return new Rectangle(getcord(x),getcord(y),1,1);
+    }
+
+    
+    @Override
+    public void dispose() {
+        generator.dispose();
+        spriteBatch.dispose();
     }
 }
--- chess/core/src/main/java/com/chess/MainScreen.java
+++ chess/core/src/main/java/com/chess/MainScreen.java
@@ -3,24 +3,70 @@ package com.chess;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import static com.chess.Main.*;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
 public class MainScreen implements Screen {
-    private SpriteBatch batch;
-    private Texture image;
+    Texture loadbutton,startbutton;
+    Rectangle loadbuttoncol = new Rectangle(getcords(7.5f, 8, 48, 16));
+    Rectangle startbuttonncol = new Rectangle(getcords(7.5f, 8, 48, 16));
+    Rectangle drawRect = new Rectangle();
+    String filepath = "";
 
     @Override
     public void show() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        spriteBatch = new SpriteBatch();
+        loadbutton = new Texture("loadbutton.png");
+        startbutton = new Texture("startbutton.png");
+        font.getData().scale(0.5f);
+
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 1000, 210);
-        batch.end();
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        baseInputLogic();
+        logic();
+        draw();
+    }
+
+    void logic() {
+        if (mouseColider.overlaps(loadbuttoncol)) {
+            if (mousedown) {
+                JFileChooser chooser = new JFileChooser(new File(System.getProperty("user.home")));
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int result = chooser.showOpenDialog(null);
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFolder = chooser.getSelectedFile();
+                    String folderPath = selectedFolder.getAbsolutePath(); // ← This is your String
+                    filepath = folderPath;
+                }
+            }
+        } else if (mouseColider.overlaps(startbuttonncol)) {
+            if (mousedown) {
+                
+            }
+        }
+    }
+
+    void draw() {
+        spriteBatch.begin();
+        drawRect = getcords(7.5f, 8, 48, 16);
+        spriteBatch.draw(loadbutton, drawRect.x, drawRect.y, drawRect.width, drawRect.height);
+        drawRect = getcords(7.5f, 30, 48, 16);
+        spriteBatch.draw(startbutton, drawRect.x, drawRect.y, drawRect.width, drawRect.height);
+        drawRect = getcords(7.5f, 48);
+        font.draw(spriteBatch, filepath, drawRect.x, drawRect.y);
+        spriteBatch.end();
     }
 
     @Override
@@ -30,6 +76,7 @@ public class MainScreen implements Screen {
         if(width <= 0 || height <= 0) return;
 
         // Resize your screen here. The parameters represent the new window size.
+        viewport.update(width, height, true);
     }
 
     @Override
@@ -49,7 +96,7 @@ public class MainScreen implements Screen {
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        loadbutton.dispose();
+        startbutton.dispose();
     }
 }
